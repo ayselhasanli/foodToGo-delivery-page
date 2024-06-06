84e43ff225a263ee77aab8059fef9549fcb75d44
ayselhasanli <aysiiihasan@gmail.com>
2024-06-06 22:50:35 +0400

    explore menu created

--- frontend/src/components/ExploreMenu/ExploreMenu.jsx
+++ frontend/src/components/ExploreMenu/ExploreMenu.jsx
@@ -1,11 +1,30 @@
-import './ExploreMenu.css'
+import "./ExploreMenu.css";
+import { menu_list } from "../../assets/assets";
 
 const ExploreMenu = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <div className="explore-menu" id="exploreMenu">
+      <h1>Explore our menu</h1>
+      <p className="explore-menu-text">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi
+        repellendus et quidem sed suscipit totam quia ducimus! Consequuntur, ea
+        sint saepe similique modi corrupti aliquam ut dignissimos maxime dolores
+        voluptatem.
+      </p>
+      <div className="explore-menu-list">
+        {menu_list.map((item, index) => {
+          return (
+            <div key={index} className="menu-list-item">
+              <img src={item.menu_image} alt="" />
+              <p>{item.menu_name}</p>
+            </div>
+          );
+        })}
+      </div>
 
-export default ExploreMenu
+      <hr />
+    </div>
+  );
+};
+
+export default ExploreMenu;
