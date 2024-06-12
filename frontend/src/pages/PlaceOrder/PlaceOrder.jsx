f150d06e699a0d893ed5db20c8c8f46e79a82e0e
ayselhasanli <aysiiihasan@gmail.com>
2024-06-12 21:27:25 +0400

    place order component created

--- frontend/src/pages/PlaceOrder/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -1,6 +1,52 @@
 import "./PlaceOrder.css";
+import { StoreContext } from "../../context/StoreContext";
+import { useContext } from "react";
+import "../Cart/Cart.css";
+
 const PlaceOrder = () => {
-  return <div></div>;
+  const { getTotalCartAmount } = useContext(StoreContext);
+  return (
+    <form className="place-order">
+      <div className="place-order-left">
+        <p className="title">Delivery Informations</p>
+        <div className="multi-fields">
+          <input type="text" placeholder="First Name" />
+          <input type="text" placeholder="Last Name" />
+        </div>
+        <input type="text" placeholder="Email adress" />
+        <input type="text" placeholder="Street" />
+        <div className="multi-fields">
+          <input type="text" placeholder="City" />
+          <input type="text" placeholder="State" />
+        </div>
+        <div className="multi-fields">
+          <input type="text" placeholder="Zip Code" />
+          <input type="text" placeholder="Country" />
+        </div>
+        <input type="text" placeholder="Phone" />
+      </div>
+      <div className="place-order-right">
+        <div className="cart-total">
+          <h2>Cart Totals</h2>
+          <div className="cart-total-details">
+            <p>Subtotal</p>
+            <p>${getTotalCartAmount()}</p>
+          </div>
+          <hr />
+          <div className="cart-total-details">
+            <p>Delivery Fee</p>
+            <p>${getTotalCartAmount() === 0 ? 0 : 2}</p>
+          </div>
+          <hr />
+          <div className="cart-total-details">
+            <b>Total</b>
+            <b>${getTotalCartAmount() === 0 ? 0 : getTotalCartAmount() + 2}</b>
+          </div>
+          <button>PROCEED TO PAYMENT</button>
+        </div>
+      </div>
+    </form>
+  );
 };
 
 export default PlaceOrder;
